8bbc6e7b2114457cb073603aaee7ad1bee275b44
GoogleFrog <googl3frog@gmail.com>
2020-04-18 18:20:15 +1000

    More pathfinder example.

--- include/pathfinder.lua
+++ include/pathfinder.lua
@@ -3,8 +3,14 @@ local function FindPath(start, goal, roomList)
 
 	local popCount = 0
 
+	for _, room in roomList.Iterator() do
+		local pos, width = room.GetPosAndWidth()
+		-- Do stuff
+	end
+
 	local externalFuncs = {}
 	
+	-- Return the next adjacent position to move towards and whether it is diagonal movement.
 	function externalFuncs.GetNextNode()
 		if popCount == 0 then
 			popCount = popCount + 1
--- room.lua
+++ room.lua
@@ -26,6 +26,10 @@ local function New(init, stationsByUse)
 		return pos
 	end
 	
+	function externalFuncs.GetPosAndWidth()
+		return pos, def.width
+	end
+	
 	function externalFuncs.Draw(offsetX, offsetY)
 		love.graphics.draw(def.image, pos[1]*GLOBAL.TILE_SIZE - offsetX, pos[2]*GLOBAL.TILE_SIZE - offsetY, 0, 1, 1, 0, 0, 0, 0)
 	end
