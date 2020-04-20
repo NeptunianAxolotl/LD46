a0fffec1c152c67d2f007e1dea43f75a8c8d982f
GoogleFrog <googl3frog@gmail.com>
2020-04-20 21:39:12 +1000

    Whoops.

--- defs/skillTypes.lua
+++ defs/skillTypes.lua
@@ -6,12 +6,14 @@ local data = {
 		titleName = "Mason",
 		desc = "Do stone",
 		learnMod = 1.7,
-		practiseMod = 1.3,
 		learnPowerDrainMod = 1.2,
 		taskSpeed = {
 			make_stone = 2,
 			build = 1.5,
-		}
+		},
+		requiredPeripherals = {
+		
+		},
 	},
 	{
 		name = "farming",
@@ -19,13 +21,15 @@ local data = {
 		titleName = "Farmer",
 		desc = "Do stone",
 		learnMod = 1.7,
-		practiseMod = 1.3,
 		learnPowerDrainMod = 1.2,
 		preferedTask = "make_veg",
 		taskSpeed = {
 			make_grain = 2,
 			make_veg = 2,
-		}
+		},
+		requiredPeripherals = {
+		
+		},
 	},
 
 }
