c5210d72847dc13830abcce108b82d1e8c531605
GoogleFrog <googl3frog@gmail.com>
2020-04-20 21:38:40 +1000

    Further pare down skillTypes.

--- defs/skillTypes.lua
+++ defs/skillTypes.lua
@@ -8,16 +8,10 @@ local data = {
 		learnMod = 1.7,
 		practiseMod = 1.3,
 		learnPowerDrainMod = 1.2,
-		requiredPerhipherals = {
-			
-		},
-		preferedTask = "make_stone",
-		EffectFunc = function (monk, rank, taskType)
-			if taskType == "make_stone" and (rank > 1.5) then
-				return (rank - 0.83)*1.5 -- Rank 1 is novice, rank 2 adept, rank 3 master. Interpolated.
-			end
-			return 1
-		end,
+		taskSpeed = {
+			make_stone = 2,
+			build = 1.5,
+		}
 	},
 	{
 		name = "farming",
@@ -28,12 +22,10 @@ local data = {
 		practiseMod = 1.3,
 		learnPowerDrainMod = 1.2,
 		preferedTask = "make_veg",
-		EffectFunc = function (monk, rank, taskType)
-			if (taskType == "make_grain" or taskType == "make_veg") and (rank > 1.5) then
-				return (rank - 0.83)*1.5 -- Rank 1 is novice, rank 2 adept, rank 3 master. Interpolated.
-			end
-			return 1
-		end,
+		taskSpeed = {
+			make_grain = 2,
+			make_veg = 2,
+		}
 	},
 
 }
